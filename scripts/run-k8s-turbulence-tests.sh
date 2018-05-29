679ccd2b4b2c0b68dc3d9f1e6c9e4ba147f178eb
Morena De Liddo <mdeliddo@pivotal.io>
2018-05-29 14:45:13 +0100

    Update locations where set_kubeconfig happens
    * Not longer source variables
    * Call function to get variables for kubeconfig
    * Call function to setup_env to access Bosh and Kubectl
    
    [#157841437]
    
    Signed-off-by: Mark James Hender <mhender@pivotal.io>

--- scripts/run-k8s-turbulence-tests.sh
+++ scripts/run-k8s-turbulence-tests.sh
@@ -9,19 +9,9 @@ KUBO_ENVIRONMENT_DIR="${ROOT}/environment"
 
 export GOPATH="${ROOT}/git-kubo-ci"
 
-setup_env() {
-  mkdir -p "${KUBO_ENVIRONMENT_DIR}"
-  cp "${ROOT}/gcs-bosh-creds/creds.yml" "${KUBO_ENVIRONMENT_DIR}/"
-  cp "${ROOT}/kubo-lock/metadata" "${KUBO_ENVIRONMENT_DIR}/director.yml"
-
-  "${ROOT}/git-kubo-deployment/bin/set_bosh_alias" "${KUBO_ENVIRONMENT_DIR}"
-  "${ROOT}/git-kubo-deployment/bin/credhub_login" "${KUBO_ENVIRONMENT_DIR}"
-  source "${ROOT}/git-kubo-ci/scripts/get_kubeconfig_vars.sh"
-  "${ROOT}/git-kubo-deployment/bin/set_kubeconfig" "${cluster_name}" "${api_url}"
-}
-
 main() {
-  setup_env
+  source "${ROOT}/git-kubo-ci/scripts/lib/utils.sh"
+  setup_env "${KUBO_ENVIRONMENT_DIR}"
 
   local tmpfile="$(mktemp)" && echo "CONFIG=${tmpfile}"
   "${ROOT}/git-kubo-ci/scripts/generate-test-config.sh" "${KUBO_ENVIRONMENT_DIR}" "${DEPLOYMENT_NAME}" > "${tmpfile}"
